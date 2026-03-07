fdf828cfd5ccdd1649bb895420f3fc4b82d03f41
raul <raulaalvarezm9@gmail.com>
2026-03-07 20:52:12 -0300

    configrando urls.py proyecto

--- servicio_tecnico/settings.py
+++ servicio_tecnico/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ztf&aycrrk72%3i#d&9%gt2uwu&k(q2#zrpvfb2^xe2w5zjo#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
