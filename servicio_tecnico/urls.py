80147661346f9091232085dde7535b98897587ec
raul <raulaalvarezm9@gmail.com>
2026-03-08 01:13:26 -0300

    configrando urls.py proyecto_debug

--- servicio_tecnico/urls.py
+++ servicio_tecnico/urls.py
@@ -18,14 +18,15 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
-#from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('services.urls')),
-]
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
 ]
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
